fe1808b37378a1d702d1cf1becf33fa53b62439c
Petter Goksøyr Åsen <boutrosboutrosboutros@gmail.com>
2014-11-29 09:14:47 +0900

    Add functionality to query a RDF repository

---
+++ repo.go
@@ -0,0 +1,103 @@
+package sparql
+
+import (
+	"bytes"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/knakk/digest"
+)
+
+// Repo represent a RDF repository, assumed to be
+// queryable via the SPARQL protocol over HTTP.
+type Repo struct {
+	endpoint string
+	client   *http.Client
+}
+
+// NewRepo creates a new representation of a RDF repository. It takes a
+// variadic list of functional options which can alter the configuration
+// of the repository.
+func NewRepo(addr string, options ...func(*Repo) error) (*Repo, error) {
+	r := Repo{
+		endpoint: addr,
+		client:   http.DefaultClient,
+	}
+	return &r, r.SetOption(options...)
+}
+
+// SetOption takes one or more option function and applies them in order to Repo.
+func (r *Repo) SetOption(options ...func(*Repo) error) error {
+	for _, opt := range options {
+		if err := opt(r); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// DigestAuth configures Repo to use digest authentication on HTTP requests.
+func DigestAuth(username, password string) func(*Repo) error {
+	return func(r *Repo) error {
+		r.client.Transport = digest.NewTransport(username, password)
+		return nil
+	}
+}
+
+// Timeout instructs the underlying HTTP transport to timeout after given duration.
+func Timeout(t time.Duration) func(*Repo) error {
+	return func(r *Repo) error {
+		r.client.Timeout = t
+		return nil
+	}
+}
+
+// Query performs a SPARQL HTTP request to the Repo, and returns the
+// parsed application/sparql-results+json response.
+func (r *Repo) Query(q string) (*Results, error) {
+	form := url.Values{}
+	form.Set("query", q)
+	form.Set("format", "application/sparql-results+json")
+	b := form.Encode()
+
+	req, err := http.NewRequest(
+		"POST",
+		s.endpoint,
+		bytes.NewBufferString(b))
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	req.Header.Set("Content-Length", strconv.Itoa(len(b)))
+
+	resp, err := r.client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		b, err := ioutil.ReadAll(resp.Body)
+		var msg string
+		if err != nil {
+			msg = "Failed to read response body"
+		} else {
+			if strings.TrimSpace(string(b)) != "" {
+				msg = "Response body: \n" + string(b)
+			}
+		}
+		return nil, fmt.Errorf("Query: SPARQL request failed: %s. "+msg, resp.Status)
+	}
+	results, err := ParseJSON(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	return results, nil
+}

a40e0e1edc576cd890488ab73d7980e4bffb05ed
jspc <james@zero-internet.org.uk>
2021-04-06 17:56:17 +0900

    Ensure max-age header does what we think it does

--- repo/opts_test.go
+++ repo/opts_test.go
@@ -1,10 +1,13 @@
 package repo
 
 import (
+	"bytes"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"net/http"
 	"testing"
+	"time"
 
 	"github.com/gregjones/httpcache"
 )
@@ -22,17 +25,24 @@ func (c *testCache) Set(key string, responseBytes []byte) {
 	c.resp = responseBytes
 }
 
-func (_ testCache) Delete(_ string) {}
+func (testCache) Delete(_ string) {}
 
 type dummyRT struct {
-	req    *http.Request
-	status int
-	err    bool
+	req      *http.Request
+	status   int
+	err      bool
+	reqCount int
+	body     []byte
 }
 
 func (r *dummyRT) RoundTrip(req *http.Request) (resp *http.Response, err error) {
+	r.reqCount += 1
 	resp = new(http.Response)
 	resp.StatusCode = r.status
+
+	if r.body != nil && len(r.body) > 0 {
+		resp.Body = io.NopCloser(bytes.NewReader(r.body))
+	}
 
 	r.req = req
 
@@ -94,5 +104,37 @@ func TestWithHeader(t *testing.T) {
 
 	if got != "world" {
 		t.Errorf(`expected "world", recived %q`, got)
+	}
+}
+
+func TestWithCacheAndWithHeader(t *testing.T) {
+	jsonBody, err := ioutil.ReadFile("../testdata/sparql_cache_response.json")
+	if err != nil {
+		t.Fatalf("unexpected error: %#v", err)
+	}
+
+	happyPathCache := &testCache{
+		resp: []byte(fmt.Sprintf(`HTTP/1.1 200
+content-type: application/sparql-results+json
+Cache-Control: max-age=30
+date: %s
+
+%s
+`, time.Now().Add(0-(time.Hour*24)).Format(http.TimeFormat), jsonBody))}
+
+	// Requests should be made, with a max age of 1 week. This should
+	// still serve requests from the cache, even though the cached response
+	// has a max age of 30 seconds, and is an hour old.
+	//
+	// Let's see.....
+	repo, _ := New("https://example.com/", WithCache(happyPathCache), WithHeader("cache-control", "max-age=86400"))
+	_, err = repo.Query("hahaha this is not real sparql <3")
+
+	// if we receive an error, which we will if the above query actually hits
+	// a real server (at the very least we expect a 404 from that example.com
+	// url) then we can assume our headers don't work- we should always get a
+	// valid response from the cache
+	if err != nil {
+		t.Errorf("unexpected error: %#v", err)
 	}
 }
